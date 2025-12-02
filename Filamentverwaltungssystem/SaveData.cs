c4bf68e89d8a509e866de8802f88b7dda82240b9
gig4ntic <guenther.simeon@gmail.com>
2025-12-02 15:38:36 +0100

    Klaasen Angelegt, User Klasse angefangen

---
+++ Filamentverwaltungssystem/SaveData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Filamentverwaltungssystem
+{
+    internal class SaveData
+    {
+    }
+}
---
+++ Filamentverwaltungssystem/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Filamentverwaltungssystem
+{
+    //Benutzerrolle Admin oder Basisnutzer
+    public enum UserRole
+    {
+        Basic,
+        Admin
+    }
+
+    internal class User
+    {
+        //Benutzername
+        public string Username { get; set; }
+        //Passwort
+        public string Password { get; set; }
+        //Rolle Admin oder Basisnutzer
+        public UserRole Role { get; set; }
+    }
+}
